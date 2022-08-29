7601b73a89efb5dd8a925a2aa3091297a0cdcd62
cassiniswingby <khigu20@gmail.com>
2022-08-29 10:44:44 +0900

    changed the type of json_input as float

--- src_mlp/inference.py
+++ src_mlp/inference.py
@@ -1,10 +1,11 @@
 import numpy as np
+import pandas as pd
 import torch
 
 
 def predict(model, json_input):
-    json_numpy = np.array(json_input)
-    json_tensor = torch.from_numpy(json_numpy).float()
+    json_numpy = np.array(json_input.astype(float))
+    json_tensor = torch.from_numpy(json_numpy)
     prediction = torch.argmax(model(json_tensor))  # model:ModelService
     print(f"prediction : {prediction}")
     return prediction
