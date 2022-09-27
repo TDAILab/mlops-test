07b8c75755899a086b41fcf43ae257fd3582815c
cassiniswingby <khigu20@gmail.com>
2022-09-27 19:08:46 +0900

    fixed refactoring

--- src_mlp/inference.py
+++ src_mlp/inference.py
@@ -4,8 +4,8 @@ import torch
 
 
 def predict(model, json_input):
-    json_numpy = np.array(json_input).astype(np.float32)
+    json_numpy = np.array(json_input).astype(np.float32)    # set float type to float32
     json_tensor = torch.from_numpy(json_numpy)
-    prediction = torch.argmax(model(json_tensor)).numpy()
+    prediction = np.array([torch.argmax(model(json_tensor))])    # specify output as np.array
     print(f"prediction : {prediction}")
     return prediction
