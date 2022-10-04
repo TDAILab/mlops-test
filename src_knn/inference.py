8f36c28615c1f9eef45f68d3ad59d1a1a997548a
cassiniswingby <khigu20@gmail.com>
2022-10-04 18:47:50 +0900

    fixed TypeError

--- src_knn/inference.py
+++ src_knn/inference.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def predict(model, input: pd.core.frame.DataFrame[float]) -> float:
+def predict(model, input: pd.core.frame.DataFrame) -> float:
     prediction = model.predict(input)
     print(f"prediction : {prediction}")
     return prediction
--- src_mlp/inference.py
+++ src_mlp/inference.py
@@ -3,7 +3,7 @@ import pandas as pd
 import torch
 
 
-def predict(model, input: pd.core.frame.DataFrame[float]) -> np.ndarray[float]:
+def predict(model, input: pd.core.frame.DataFrame) -> np.ndarray:
     input_numpy = np.array(input).astype(np.float32)    # set float type to float32
     input_tensor = torch.from_numpy(input_numpy)
     prediction = np.array([torch.argmax(model(input_tensor))])    # specify output as np.array
